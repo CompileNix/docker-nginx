b42f8fd1278edf548452d10fd8c4093e0cab2ec8
CompileNix <compilenix@gmail.com>
2022-09-28 20:04:00 +0200

    update envsubst shell script
    remove some double-slashes in paths. no logic change

--- src/docker-entrypoint.d/90-envsubst-on-templates.sh
+++ src/docker-entrypoint.d/90-envsubst-on-templates.sh
@@ -5,35 +5,30 @@ set -eu
 
 ME=$(basename $0)
 
-auto_envsubst() {
-  local templates="/etc/nginx/ ${NGINX_ENVSUBST_TEMPLATES:-}"
-  local suffix=".conf"
+templates="/etc/nginx ${NGINX_ENVSUBST_TEMPLATES:-}"
+suffix="$NGINX_ENVSUBST_TEMPLATE_SUFFIX"
 
-  local template template_location defined_envs relative_path output_path subdir dir_name
-  defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
-  echo >&3 "$ME: Copy nginx config files to temp dir..."
-  for template_location in $templates; do
-    if [ -d "$template_location" ] && [ ! -z "$(ls -A $template_location)" ]; then
-      mkdir -pv "/tmp/nginx-envsubst/conf/${template_location#/etc/nginx/}"
-      cp -rv $template_location/* "/tmp/nginx-envsubst/conf/${template_location#/etc/nginx/}/"
-    fi
-    if [ -f "$template_location" ]; then
-      dir_name="$(dirname $template_location)"
-      mkdir -pv "/tmp/nginx-envsubst/conf${dir_name#/etc/nginx}/"
-      cp -v "$template_location" "/tmp/nginx-envsubst/conf${dir_name#/etc/nginx}/"
-    fi
+defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
+echo >&3 "$ME: Copy nginx config files to temp dir..."
+for template_location in $templates; do
+  if [ -d "$template_location" ] && [ ! -z "$(ls -A $template_location)" ]; then
+    mkdir -pv "/tmp/nginx-envsubst/${template_location#/etc/nginx}"
+    cp -rv $template_location/* "/tmp/nginx-envsubst/${template_location#/etc/nginx}"
+  fi
+  if [ -f "$template_location" ]; then
+    dir_name="$(dirname $template_location)"
+    mkdir -pv "/tmp/nginx-envsubst${dir_name#/etc/nginx}/"
+    cp -v "$template_location" "/tmp/nginx-envsubst${dir_name#/etc/nginx}/"
+  fi
+done
+
+for template_file in $(find "/tmp/nginx-envsubst/" -follow -type f -name "*$suffix" -print); do
+  echo $template_file | while read -r template; do
+    echo >&3 "$ME: Running envsubst for /etc/nginx/${template_file#/tmp/nginx-envsubst/}"
+    envsubst "$defined_envs" <"$template" >"/etc/nginx/${template_file#/tmp/nginx-envsubst/}"
   done
+done
 
-  for template_file in $(find "/tmp/nginx-envsubst/conf/" -follow -type f -name "*$suffix" -print); do
-    echo $template_file | while read -r template; do
-      echo >&3 "$ME: Running envsubst for /etc/nginx/${template_file#/tmp/nginx-envsubst/conf/}"
-      envsubst "$defined_envs" <"$template" >"/etc/nginx/${template_file#/tmp/nginx-envsubst/conf/}"
-    done
-  done
-
-  echo >&3 "$ME: Cleanup nginx config files in temp dir..."
-  rm -rf "/tmp/nginx-envsubst"
-}
-
-auto_envsubst
+echo >&3 "$ME: Cleanup nginx config files in temp dir..."
+rm -rf "/tmp/nginx-envsubst"
 
